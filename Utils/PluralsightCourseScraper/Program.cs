9a316b3432d9dc61c9c98633ab770b946a5bf1d4
Simon Cromwell <cromwellsimon@gmail.com>
2024-05-09 17:16:59 -0400

    Final changes to the PluralsightCourseScraper

--- Utils/PluralsightCourseScraper/Program.cs
+++ Utils/PluralsightCourseScraper/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -7,92 +6,85 @@ using System.Xml.Linq;
 
 namespace PluralsightCourseScraper;
 
-// Making this file as a demonstration to a friend of mine, the one that I'll actually use is the version titled "Program2"
+/// <summary>
+/// This will take in the contents of PluralsightProfile.html and parse out all of the Completed Courses from it into the Course record as specified in my Website.Shared.
+/// The completed file will be called ParsedCourses.txt and it should be good to just Ctrl+A, Ctrl+C, and Ctrl+V.
+/// The file gets output to the same folder as your executable. If you are running this in debug, it can be found under bin/Debug/net8.0 at the time of writing since that is where the executable gets placed.
+/// </summary>
 internal class Program
 {
+    /// <summary>
+    /// The class ID used for the relevant anchor links.
+    /// Because Pluralsight uses React.js, a class name is programmatically generated for some queries and CSS isolation.
+    /// </summary>
+    const string className = "c66719c022472e356045";
+    /// <summary>
+    /// The path name of the file that contains the saved Pluralsight Profile.
+    /// In order to generate this file, all you have to do is go to a Pluralsight profile, expand all of the completed courses, hit F12, go to Elements,
+    /// then scroll to the top <html> tag, right-click and select "Edit as HTML", then Ctrl+A, Ctrl+C, Ctrl+V into PluralsightProfile.html.
+    /// I tried doing this all through just sending a GET request to the site itself but, after skirting around CORS issues, I was not able to get the info I needed through just that.
+    /// </summary>
+    static string pluralsightProfilePath = "PluralsightProfile.html";
+    const string pluralsightBaseUrl = "https://app.pluralsight.com";
+    /// <summary>
+    /// The text that the line for the relevant HTML starts with.
+    /// XElement obviously can't parse JavaScript or CSS so putting this here is necessary in order to actually find the relevant HTML
+    /// </summary>
+    const string appStart = """<div id="app">""";
+    /// <summary>
+    /// The name of the file to put the final list of Courses. This can be anything.
+    /// As stated above, this will be found in the same folder as your executable which means that it would be in bin/Debug/net8.0 if running in debug.
+    /// </summary>
+    static string parsedCoursesPath = "ParsedCourses.txt";
+
     static void Main(string[] args)
     {
-        string[] lines = File.ReadAllLines(args[0]);
-        string foundLine = "";
-        int index = 0;
-        while (index < lines.Length)
+        if (args.Length >= 1)
         {
-            string line = lines[index];
-            if (line.StartsWith("""<div id="app">"""))
-            {
-                foundLine = line;
-                break;
-            }
-            index = index + 1;
+            pluralsightProfilePath = args[0];
         }
-        XElement profileElement = XElement.Parse(foundLine);
-        var foundElements = profileElement.Descendants("a");
-        List<XElement> validElements = foundElements
+        if (args.Length >= 2)
+        {
+            parsedCoursesPath = args[1];
+        }
+        string? pluralsightProfileText = null;
+        using (StreamReader pluralsightProfileFile = File.OpenText(pluralsightProfilePath))
+        {
+            while (string.IsNullOrWhiteSpace(pluralsightProfileText))
+            {
+                string? line = pluralsightProfileFile.ReadLine();
+                if (line != null && line.StartsWith(appStart))
+                {
+                    pluralsightProfileText = line;
+                }
+            }
+        }
+        if (string.IsNullOrWhiteSpace(pluralsightProfileText) )
+        {
+            Console.WriteLine($"{appStart} could not be found at the start of any lines in {pluralsightProfilePath}. Make sure that it is set properly!");
+        }
+
+        XElement pluralsightProfile = XElement.Parse(pluralsightProfileText);
+        var validElements = pluralsightProfile.Descendants("a")
             .Where((element) =>
             {
-                XAttribute attribute = element.Attribute("class");
-                return attribute != null && attribute.Value.Contains("c66719c022472e356045");
-            })
-            .ToList();
-        List<Course> courses = new List<Course>();
+                if (element.Attribute("class") is XAttribute attribute && attribute.Value.StartsWith(className))
+                {
+                    return true;
+                }
+                return false;
+            });
+        
+        if (validElements.Any() == false) 
+        {
+            Console.WriteLine("No elements could be found");
+            return;
+        }
+        StringBuilder pluralsightCourseData = new();
         foreach (XElement element in validElements)
         {
-            Course course = new Course();
-            course.name = element.Value;
-            course.url = $"https://app.pluralsight.com{element.Attribute("href").Value}";
-            courses.Add(course);
+            pluralsightCourseData.AppendLine($@"new(""{element.Value}"", ""{pluralsightBaseUrl}{element.Attribute("href")!.Value}""),");
         }
-        foreach (Course course in courses)
-        {
-            string finalString = $"{course.name} {course.url}";
-        }
-    }
-}
-
-/// <summary>
-/// Funny description here
-/// </summary>
-public class Course
-{
-    public string name;
-    public string url;
-
-    public void PrintUrl(string newUrl)
-    {
-        url = newUrl;
-        Console.WriteLine(newUrl);
-    }
-
-    public string GetName()
-    {
-        return name;
-    }
-
-    // """name,url"""
-    public static Course Parse(string course)
-    {
-        string[] splitCourse = course.Split(',');
-        Course newCourse = new Course();
-        newCourse.name = splitCourse[0];
-        newCourse.url = splitCourse[1];
-        return newCourse;
-    }
-}
-
-public static class CourseExtensions
-{
-    public static string GetUrl(this Course course)
-    {
-        return course.url;
-    }
-
-    public static bool GetValidAnchors(XElement element)
-    {
-        XAttribute attribute = element.Attribute("class");
-        if (attribute != null && attribute.Value.Contains("c66719c022472e356045"))
-        {
-            return true;
-        }
-        return false;
+        File.WriteAllText(parsedCoursesPath, pluralsightCourseData.ToString());
     }
 }
--- Utils/PluralsightCourseScraper/Program2.cs
+++ Utils/PluralsightCourseScraper/Program2.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -6,77 +7,92 @@ using System.Xml.Linq;
 
 namespace PluralsightCourseScraper;
 
-/// <summary>
-/// This will take in the contents of PluralsightProfile.html and parse out all of the Completed Courses from it into the Course record as specified in my Website.Shared.
-/// The completed file will be called ParsedCourses.txt and it should be good to just Ctrl+A, Ctrl+C, and Ctrl+V.
-/// The file gets output to the same folder as your executable. If you are running this in debug, it can be found under bin/Debug/net8.0 at the time of writing since that is where the executable gets placed.
-/// </summary>
+// Making this file as a demonstration to a friend of mine, the one that I'll actually use is the version titled "Program2"
 internal class Program2
 {
-    /// <summary>
-    /// The class ID used for the relevant anchor links.
-    /// Because Pluralsight uses React.js, a class name is programmatically generated for some queries and CSS isolation.
-    /// </summary>
-    const string className = "c66719c022472e356045";
-    /// <summary>
-    /// The path name of the file that contains the saved Pluralsight Profile.
-    /// In order to generate this file, all you have to do is go to a Pluralsight profile, expand all of the completed courses, hit F12, go to Elements,
-    /// then scroll to the top <html> tag, right-click and select "Edit as HTML", then Ctrl+A, Ctrl+C, Ctrl+V into PluralsightProfile.html.
-    /// I tried doing this all through just sending a GET request to the site itself but, after skirting around CORS issues, I was not able to get the info I needed through just that.
-    /// </summary>
-    const string pluralsightProfilePath = "PluralsightProfile.html";
-    const string pluralsightBaseUrl = "https://app.pluralsight.com";
-    /// <summary>
-    /// The text that the line for the relevant HTML starts with.
-    /// XElement obviously can't parse JavaScript or CSS so putting this here is necessary in order to actually find the relevant HTML
-    /// </summary>
-    const string appStart = """<div id="app">""";
-    /// <summary>
-    /// The name of the file to put the final list of Courses. This can be anything.
-    /// As stated above, this will be found in the same folder as your executable which means that it would be in bin/Debug/net8.0 if running in debug.
-    /// </summary>
-    const string parsedCoursesPath = "ParsedCourses.txt";
-
     static void Main2(string[] args)
     {
-        string? pluralsightProfileText = null;
-        using (StreamReader pluralsightProfileFile = File.OpenText(pluralsightProfilePath))
+        string[] lines = File.ReadAllLines(args[0]);
+        string foundLine = "";
+        int index = 0;
+        while (index < lines.Length)
         {
-            while (string.IsNullOrWhiteSpace(pluralsightProfileText))
+            string line = lines[index];
+            if (line.StartsWith("""<div id="app">"""))
             {
-                string? line = pluralsightProfileFile.ReadLine();
-                if (line != null && line.StartsWith(appStart))
-                {
-                    pluralsightProfileText = line;
-                }
+                foundLine = line;
+                break;
             }
+            index = index + 1;
         }
-        if (string.IsNullOrWhiteSpace(pluralsightProfileText) )
-        {
-            Console.WriteLine($"{appStart} could not be found at the start of any lines in PluralsightProfile.html. Make sure that it is set properly!");
-        }
-
-        XElement pluralsightProfile = XElement.Parse(pluralsightProfileText);
-        var validElements = pluralsightProfile.Descendants("a")
+        XElement profileElement = XElement.Parse(foundLine);
+        var foundElements = profileElement.Descendants("a");
+        List<XElement> validElements = foundElements
             .Where((element) =>
             {
-                if (element.Attribute("class") is XAttribute attribute && attribute.Value.StartsWith(className))
-                {
-                    return true;
-                }
-                return false;
-            });
-        
-        if (validElements.Any() == false) 
-        {
-            Console.WriteLine("No elements could be found");
-            return;
-        }
-        StringBuilder pluralsightCourseData = new();
+                XAttribute attribute = element.Attribute("class");
+                return attribute != null && attribute.Value.Contains("c66719c022472e356045");
+            })
+            .ToList();
+        List<Course> courses = new List<Course>();
         foreach (XElement element in validElements)
         {
-            pluralsightCourseData.AppendLine($@"new(""{element.Value}"", ""{pluralsightBaseUrl}{element.Attribute("href")!.Value}""),");
+            Course course = new Course();
+            course.name = element.Value;
+            course.url = $"https://app.pluralsight.com{element.Attribute("href").Value}";
+            courses.Add(course);
         }
-        File.WriteAllText(parsedCoursesPath, pluralsightCourseData.ToString());
+        foreach (Course course in courses)
+        {
+            string finalString = $"{course.name} {course.url}";
+        }
+    }
+}
+
+/// <summary>
+/// Funny description here
+/// </summary>
+public class Course
+{
+    public string name;
+    public string url;
+
+    public void PrintUrl(string newUrl)
+    {
+        url = newUrl;
+        Console.WriteLine(newUrl);
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    // """name,url"""
+    public static Course Parse(string course)
+    {
+        string[] splitCourse = course.Split(',');
+        Course newCourse = new Course();
+        newCourse.name = splitCourse[0];
+        newCourse.url = splitCourse[1];
+        return newCourse;
+    }
+}
+
+public static class CourseExtensions
+{
+    public static string GetUrl(this Course course)
+    {
+        return course.url;
+    }
+
+    public static bool GetValidAnchors(XElement element)
+    {
+        XAttribute attribute = element.Attribute("class");
+        if (attribute != null && attribute.Value.Contains("c66719c022472e356045"))
+        {
+            return true;
+        }
+        return false;
     }
 }
